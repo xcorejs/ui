320c62d3307314a0cce2513550c39295b5581dca
tomasnyvlt <tomas@nyvlt.xyz>
2020-02-12 14:10:48 +0100

    Container types - fluid | normal

--- stories/Container.stories.tsx
+++ stories/Container.stories.tsx
@@ -1,26 +1,47 @@
 import { Box, Container, container, createTheme } from '../src';
-import React, { FC } from 'react';
+import React, { FC, Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 export default { title: 'Container' };
 
 export const BasicUsage: FC = () => (
-  <Container>
-    <Box width="100%">
-      <Box background="crimson" color="white" p="10px" my="10px">
-        Box 1
+  <Fragment>
+    <Container>
+      <Box width="100%">
+        <Box background="crimson" color="white" p="10px" my="10px">
+          Box 1
+        </Box>
+        <Box background="navy" color="white" p="10px" my="10px">
+          Box 2
+        </Box>
       </Box>
-      <Box background="navy" color="white" p="10px" my="10px">
-        Box 2
+    </Container>
+
+    <Container type="fluid">
+      <Box width="100%">
+        <Box background="crimson" color="white" p="10px" my="10px">
+          fluid
+        </Box>
+        <Box background="navy" color="white" p="10px" my="10px">
+          Box 2
+        </Box>
       </Box>
-    </Box>
-  </Container>
+    </Container>
+  </Fragment>
 );
 
 const theme = createTheme({
   name: 'Container theme',
   ...container({
-    width: '70%'
+    types: {
+      normal: {
+        width: ['100%', '100%', '30rem', '40rem', '50rem', '70rem'],
+        background: 'grey'
+      },
+      fluid: {
+        background: 'red'
+      }
+    }
   })
 });
 
