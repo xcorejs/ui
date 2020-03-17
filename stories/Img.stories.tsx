a7ec733bde04475018fc9869fb1a8facb64c8897
Denis Homolík <alfonz@homolik.cz>
2020-03-17 09:42:10 +0100

    Add objectFit prop to Img component

--- stories/Img.stories.tsx
+++ stories/Img.stories.tsx
@@ -7,7 +7,7 @@ export default { title: 'Img' };
 export const BasicUsage: FC = () => {
   return (
     <Stack gap="30px" dir="row" justifyContent="flex-start" alignItems="flex-start">
-      <Img src="http://placekitten.com/200/300" alt="" />
+      <Img src="http://placekitten.com/200/300" alt="" width="100%" objectFit="contain" />
       <Img src="http://placekitten.com/400/500" alt="" />
       <Img src="http://placekitten.com/400/200" alt="" />
     </Stack>
