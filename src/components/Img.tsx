447170d2344b9821fe26b4055c5dfa39298e1031
Denis Homolík <alfonz@homolik.cz>
2020-03-17 09:45:10 +0100

    Add objectFit prop to Img component (#62)

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -1,16 +1,30 @@
 import React, { FC, ImgHTMLAttributes } from 'react';
-import Box, { BoxProps } from './Box';
+import { BoxProps } from './Box';
+import { ResponsiveValue, system } from 'styled-system';
+import * as CSS from 'csstype';
+import styled from 'styled-components';
+import { compose } from '../utils/baseStyle';
+import { boxBase } from '../bases';
 
 export type ImgProps =
   {
     src: string;
     alt: string;
+    objectFit?: ResponsiveValue<CSS.ObjectFitProperty>;
   }
   & ImgHTMLAttributes<HTMLImageElement>
   & BoxProps;
 
 const Img: FC<ImgProps> = ({ ...props }) =>
-  <Box as="img" {...props} />
+  <ImgStyle {...props} />
 ;
 
 export default Img;
+
+const ImgStyle = styled.img<ImgProps>`
+  ${compose(boxBase)}
+
+  ${system({
+    objectFit: true
+  })}
+`;
