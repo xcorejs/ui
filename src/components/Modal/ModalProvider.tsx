30edae1ee651551fac6c86f98b710297b7fb2b12
Denis Homolík <alfonz@homolik.cz>
2020-05-03 19:15:51 +0200

    Add Modal history (#82)

--- src/components/Modal/ModalProvider.tsx
+++ src/components/Modal/ModalProvider.tsx
@@ -1,16 +1,39 @@
-import React, { FC, useState, createElement } from 'react';
+import React, { createElement, FC, useState, ComponentType } from 'react';
 
-import { ModalContext, ModalType } from './data';
+import { ModalContext, ModalInstanceContext } from './data';
 
 const ModalProvider: FC = ({ children }) => {
-  const [[modal], setModal] = useState<[ModalType] | []>([]);
+  const [position, setPosition] = useState(-1);
+  const [modals, setModal] = useState<ComponentType[]>([]);
+
+  const context: ModalContext = {
+    push: m => {
+      setModal(v => [...v, m]);
+      setPosition(p => p + 1);
+    },
+    replace: m => {
+      setModal(v => [...v.slice(0, -1), m]);
+      setPosition(p => p + 1);
+    },
+
+    pop: () => {
+      setModal(v => v.slice(0, -1));
+      setPosition(p => p - 1);
+    },
+
+    go: (n: number) => setPosition(p => p + n),
+    back: () => setPosition(p => p - 1),
+    forward: () => setPosition(p => p + 1)
+  };
 
   return (
-    <ModalContext.Provider
-      value={{ setModal: (m) => setModal([m]) }}
-    >
+    <ModalContext.Provider value={context}>
       {children}
-      {modal && createElement(modal, { close: () => setModal([]) })}
+      {position !== -1 && modals.map((m, i) => (
+        <ModalInstanceContext.Provider key={i} value={{ active: i === position }}>
+          {createElement(m)}
+        </ModalInstanceContext.Provider>
+      ))}
     </ModalContext.Provider>
   );
 };
