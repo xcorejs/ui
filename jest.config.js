6538b5a150f489adac192568a2c276e8cce1ef3e
Denis Homolik <alfonz@homolik.cz>
2021-02-26 23:59:56 +0100

    Add jest snapshot tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@xcorejs/ui': '<rootDir>/src'
+  },
+  moduleDirectories: ['node_modules', 'src']
+};
---
+++ src/components/Card/index.test.tsx
@@ -0,0 +1,62 @@
+import 'jest-styled-components';
+
+import { Card } from '.';
+import { x } from "@xstyled/styled-components";
+import React, { FC } from 'react';
+import renderer from 'react-test-renderer';
+
+test('Card component', () => {
+  const component = renderer.create(
+    <>
+      <Card
+        title="Card title"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        footer={<x.p>Footer</x.p>}
+      >
+        <LoremIpsum units="sentences" />
+      </Card>
+
+      <Card
+        title="Card title"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        body={<LoremIpsum units="sentences" />}
+        footer={<x.p>Footer</x.p>}
+      />
+
+      <Card
+        title="Card title"
+        tag="Tag"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        body={<LoremIpsum units="sentences" />}
+      />
+
+      <Card
+        title="Card title"
+        tag="Tag"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        body={<LoremIpsum units="sentences" />}
+      />
+
+      <Card
+        tag="Tag"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        body={<LoremIpsum units="sentences" />}
+      />
+
+      <Card
+        title="Thick"
+        tag="Tag"
+        media={<x.img src="http://placekitten.com/500/300" alt="cat" />}
+        body={<LoremIpsum units="sentences" />}
+      />
+    </>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+const LoremIpsum: FC<any> = () => {
+  return (
+    <>text text text</>
+  );
+};
